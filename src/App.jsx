a64a151411f9ea36bc22e3d79d8f66d4efb3fe5e
EmeraldNV <abdellah316619@gmail.com>
2026-02-24 19:09:01 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,18 @@ import { PersistGate } from "redux-persist/integration/react";
 import ProfilePage from "./components/ProfilePage";
 import HomepageFrontBanner from "./components/HomepageFrontBanner/HomepageFrontBanner";
 import ProfileLeftSidebarComponent from "./components/ProfileLeftSidebarComponent/ProfileLeftSidebarComponent";
+import UniversalMessagingComponent from "./components/UniversalMessagingComponent/UniversalMessagingComponent";
 
 function App() {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistedStore}>
+<<<<<<< Updated upstream
         <ProfilePage />
+=======
+        <ProfileLeftSidebarComponent />
+        <UniversalMessagingComponent />
+>>>>>>> Stashed changes
       </PersistGate>
     </Provider>
   );
